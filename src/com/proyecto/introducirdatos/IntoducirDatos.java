beaf96b370e2e484f96266fab498c39b4a1da412
arturo <arturo@ArturoMSI>
2018-02-26 09:57:42 +0100

    Introducir datos a traves de la libreria

---
+++ src/com/proyecto/introducirdatos/IntoducirDatos.java
@@ -0,0 +1,30 @@
+package com.proyecto.introducirdatos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Arturo
+ */
+public class IntoducirDatos {
+    /**
+     * Metodo para pedir datos String
+     * 
+     * @param mensage mensaje a mostrar
+     * @return dato recogido
+     */
+    public String introducirString(String mensage){
+        String m=JOptionPane.showInputDialog(mensage);
+        return m;
+    }
+    /**
+     * Metodo para pedir datos Int
+     * 
+     * @param mensage mensaje a mostrar
+     * @return dato recogido
+     */
+    public int introducirInt(String mensage){
+        int dato=Integer.parseInt(JOptionPane.showInputDialog(mensage));
+        return dato;
+    }
+}
