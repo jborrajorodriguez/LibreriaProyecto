924f6f0ad22d5aa85e9c9a5d3c77daf2fab22887
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-26 09:55:05 +0100

    Metodo mostrar un mensaje

--- src/com/proyecto/display/Display.java
+++ src/com/proyecto/display/Display.java
@@ -1,7 +1,17 @@
 package com.proyecto.display;
+
+import javax.swing.JOptionPane;
+
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class Display {
+    /**
+     * Método mostrar mensaje.
+     * @param mensaje de tipo String.Es el mensaje que se quiere mostrar.
+     */
+    public static void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(null,mensaje);
+    }
 
 }
