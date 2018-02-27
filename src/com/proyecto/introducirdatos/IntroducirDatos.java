3ee770422aade6dd79c9527e2024e1cd84a90b44
arturo <arturo@ArturoMSI>
2018-02-27 09:10:03 +0100

    Añadido metodo para introducir un boolean

--- src/com/proyecto/introducirdatos/IntroducirDatos.java
+++ src/com/proyecto/introducirdatos/IntroducirDatos.java
@@ -6,25 +6,38 @@ import javax.swing.JOptionPane;
  *
  * @author Arturo
  */
-public class IntroducirDatos {
+public class IntroducirDatos{
+
     /**
      * Metodo para pedir datos String
-     * 
-     * @param mensage mensaje a mostrar
+     *
+     * @param mensaje mensaje a mostrar
      * @return dato recogido
      */
-    public static String introducirString(String mensage){
-        String m=JOptionPane.showInputDialog(mensage);
+    public static String introducirString(String mensaje){
+        String m=JOptionPane.showInputDialog(mensaje);
         return m;
     }
+
     /**
      * Metodo para pedir datos Int
-     * 
-     * @param mensage mensaje a mostrar
+     *
+     * @param mensaje mensaje a mostrar
      * @return dato recogido
      */
-    public static int introducirInt(String mensage){
-        int dato=Integer.parseInt(JOptionPane.showInputDialog(mensage));
+    public static int introducirInt(String mensaje){
+        int dato=Integer.parseInt(JOptionPane.showInputDialog(mensaje));
         return dato;
+    }
+
+    /**
+     * Metodo para pedir datos Boolean
+     *
+     * @param mensaje mensaje a mostrar
+     * @return dato recogido
+     */
+    public static boolean introducirBoolean(String mensaje){
+        boolean b=Boolean.parseBoolean(JOptionPane.showInputDialog(mensaje));
+        return b;
     }
 }
